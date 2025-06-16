ec0f9de814483b503da2a144db2b37660adb00fd
selma_bettaieb <sbettaie56@gmail.com>
2025-06-16 15:16:25 +0100

    last fix

--- app/firebase.ts
+++ app/firebase.ts
@@ -2,14 +2,14 @@ import { initializeApp } from 'firebase/app';
 import { getDatabase } from 'firebase/database';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCeYDk4JYffPpwJXWD-edYqn9CjCpXtpUE",
-  authDomain: "twise-feedback.firebaseapp.com",
-  databaseURL: "https://twise-feedback-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "twise-feedback",
-  storageBucket: "twise-feedback.firebasestorage.app",
-  messagingSenderId: "349226192684",
-  appId: "1:349226192684:web:1fa691d13cfbe6c8aee689",
-  measurementId: "G-5YVC4K1SQ4"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY || "AIzaSyCeYDk4JYffPpwJXWD-edYqn9CjCpXtpUE",
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN || "twise-feedback.firebaseapp.com",
+  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL || "https://twise-feedback-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID || "twise-feedback",
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET || "twise-feedback.firebasestorage.app",
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID || "349226192684",
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID || "1:349226192684:web:1fa691d13cfbe6c8aee689",
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID || "G-5YVC4K1SQ4"
 };
 
 const app = initializeApp(firebaseConfig);
